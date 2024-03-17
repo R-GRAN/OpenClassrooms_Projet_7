f836ba6099b5ce033e31cbca7f8690ff4fe81334
Remy <remy.grangenois@gmail.com>
2024-03-17 20:20:07 +0100

    Headers CORS ok

--- app.js
+++ app.js
@@ -10,7 +10,15 @@ mongoose.connect('mongodb+srv://openClassrooms:openClassrooms@cluster0.kbbytib.m
 
 const app = express();
 
-app.use((req, res) => {
+/* headers CORS */
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
+app.get("/api/books",(req, res) => {
    res.json({ message: "hello c'est fait !" }); 
 });
 
