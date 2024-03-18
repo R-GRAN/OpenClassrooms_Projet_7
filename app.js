b6064a73c81a5f090247bdc24df61ad4d198b902
Remy <remy.grangenois@gmail.com>
2024-03-18 04:08:31 +0100

    Parsage du json dans app pour récuperer le body, correctif du port 3000 en 4000, routes OK

--- app.js
+++ app.js
@@ -1,25 +1,37 @@
-const express = require('express');
-const mongoose = require("mongoose")
+const express = require("express");
+const mongoose = require("mongoose");
+const userRoutes = require("./routes/user");
 
-
-mongoose.connect('mongodb+srv://openClassrooms:openClassrooms@cluster0.kbbytib.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
-  { useNewUrlParser: true,
-    useUnifiedTopology: true })
-  .then(() => console.log('Connexion à MongoDB réussie !'))
-  .catch(() => console.log('Connexion à MongoDB échouée !'));
+mongoose
+  .connect(
+    "mongodb+srv://openClassrooms:openClassrooms@cluster0.kbbytib.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
 
 const app = express();
 
 /* headers CORS */
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
-  });
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+/* Permet de recuperer le JSON (ancien bodyParser) */
+app.use(express.json())
 
-app.get("/api/books",(req, res) => {
-   res.json({ message: "hello c'est fait !" }); 
+app.use("/api/auth", userRoutes);
+
+app.get("/api/books", (req, res) => {
+  res.json({ message: "hello c'est fait !" });
 });
 
 module.exports = app;
