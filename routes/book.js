85d28de3b00e4197ac8ccda32a406324b275d2aa
Remy <remy.grangenois@gmail.com>
2024-03-24 11:01:46 +0100

    Installation et mise en place de multer

--- routes/book.js
+++ routes/book.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const bookCtrl = require("../controllers/book");
-const auth = require("../middlewares/auth")
+const auth = require("../middlewares/auth");
+const multer = require("../middlewares/multer-config");
 
 router.get("/", bookCtrl.getAllBooks);
 
