16ddef5c40eca3a7b26bcc352a30f5aec8fcb4e2
Remy <remy.grangenois@gmail.com>
2024-03-29 16:31:10 +0100

    sharp parametré

--- routes/book.js
+++ routes/book.js
@@ -2,17 +2,17 @@ const express = require("express");
 const router = express.Router();
 const bookCtrl = require("../controllers/book");
 const auth = require("../middlewares/auth");
-const multer = require("../middlewares/multer-config");
+const { upload, imageMiddleware } = require("../middlewares/multer-config");
 
 router.get("/", bookCtrl.getAllBooks);
 router.get("/bestrating", bookCtrl.getBestRatingBooks);
 router.get("/:id", bookCtrl.getBookById);
 
-router.post("/", auth, multer, bookCtrl.createBook);
-router.post("/:id/rating", auth,bookCtrl.createRatingBook)
+router.post("/", auth, upload, imageMiddleware, bookCtrl.createBook);
+router.post("/:id/rating", auth, bookCtrl.createRatingBook);
 
-router.put("/:id", auth, multer, bookCtrl.modifyBook);
+router.put("/:id", auth, upload, imageMiddleware, bookCtrl.modifyBook);
 
-router.delete("/:id", auth, multer, bookCtrl.deleteBook);
+router.delete("/:id", auth, bookCtrl.deleteBook);
 
 module.exports = router;
