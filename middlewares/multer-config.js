3393a937c3b01fb7aef0e97a1e592037a0a3a04e
Remy <remy.grangenois@gmail.com>
2024-03-29 19:58:59 +0100

    suppression de l'image lors de la modification ou la suppression du book

--- middlewares/multer-config.js
+++ middlewares/multer-config.js
@@ -1,7 +1,7 @@
-const multer = require('multer');
-const sharp = require('sharp');
-const path = require('path');
-const fs = require('fs').promises; // Utiliser fs.promises pour les fonctions asynchrones
+const multer = require("multer");
+const sharp = require("sharp");
+const path = require("path");
+const fs = require("fs").promises; // Utiliser fs.promises pour les fonctions asynchrones
 
 // Configuration de Multer pour le stockage des fichiers téléchargés
 const storage = multer.memoryStorage();
@@ -10,23 +10,19 @@ const upload = multer({ storage: storage });
 // Middleware pour le téléchargement de l'image, le redimensionnement et le stockage
 const imageMiddleware = async (req, res, next) => {
   try {
-    // Vérification si le fichier est une image
-    if (!req.file || !req.file.mimetype.startsWith('image')) {
-      return res.status(400).send('Veuillez télécharger une image.');
-    }
+    if (!req.file) return next();
 
     // Redimensionnement de l'image avec Sharp
     const resizedImageBuffer = await sharp(req.file.buffer)
       .resize({ width: 400 }) // Spécifier la largeur souhaitée
-      .toFormat('webp') // Convertir en format WebP
+      .toFormat("webp") // Convertir en format WebP
       .toBuffer();
 
     // Génération d'un nom de fichier unique avec l'extension .webp
     const name = req.file.originalname.split(" ").join("_");
-    const fileName = `${name+Date.now()}.webp`;
+    const fileName = `${name + Date.now()}.webp`;
     // Définir le chemin où enregistrer l'image (à la racine du projet dans le dossier "images")
-    const filePath = path.join(__dirname, '..', 'images', fileName); // ".." pour revenir au répertoire parent
-
+    const filePath = path.join(__dirname, "..", "images", fileName); // ".." pour revenir au répertoire parent
     // Écriture de l'image redimensionnée dans le dossier spécifié
     await fs.writeFile(filePath, resizedImageBuffer);
 
@@ -34,13 +30,15 @@ const imageMiddleware = async (req, res, next) => {
     req.file.filename = fileName;
     next();
   } catch (error) {
-    console.error('Erreur lors du traitement de l\'image :', error);
-    return res.status(500).send('Une erreur est survenue lors du traitement de l\'image.');
+    console.error("Erreur lors du traitement de l'image :", error);
+    return res
+      .status(500)
+      .send("Une erreur est survenue lors du traitement de l'image.");
   }
 };
 
 // Export du middleware d'upload d'image
 module.exports = {
-  upload: upload.single('image'), // Middleware de téléchargement d'image
-  imageMiddleware: imageMiddleware // Middleware de redimensionnement et stockage d'image
+  upload: upload.single("image"), // Middleware de téléchargement d'image
+  imageMiddleware: imageMiddleware, // Middleware de redimensionnement et stockage d'image
 };
